217e1aa8c223c2efbbf0ef49ec296cbb1eabcf08
Ramon <ramonjscorrea@gmail.com>
2025-01-08 19:42:46 -0300

    Deixando a NavBar responsiva

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,17 +1,30 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import "../styles/Nav.css";
 import logo from "../assets/images/logo.png";
 
 const Nav = () => {
+  const [menuOpen, setMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setMenuOpen(!menuOpen);
+  };
+
   return (
     <header>
+      
       <div className="image-logo">
         <img src={logo} alt="Logo" />
       </div>
+
+      <div className="hamburger" onClick={toggleMenu}>
+        <span></span>
+        <span></span>
+        <span></span>
+      </div>
       
       <nav>
-        <ul>
+        <ul className={menuOpen ? "active" : ""}>
           <li><Link to="/">Home</Link></li>
           <li><Link to="/about">About</Link></li>
           <li><Link to="/menu">Menu</Link></li>
@@ -20,7 +33,7 @@ const Nav = () => {
         </ul>
       </nav>
 
-      <button>order now</button>
+      <button>Order Now</button>
     </header>
   );
 };
